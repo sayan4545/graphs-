d09d9986a9657e1efa840426e7b812fe1d3270ba
sayan4545 <sayanchatterjee3010@gmail.com>
2025-07-09 22:05:27 +0530

    Adjacency list implementation and printing any nodes neighbours in unweighted undirected graph

---
+++ src/main/java/com/dev/springboot/bootcamp01/graphImplementations/AdjacencyListImpl.java
@@ -0,0 +1,41 @@
+package com.dev.springboot.bootcamp01.graphImplementations;
+
+import java.util.ArrayList;
+
+public class AdjacencyListImpl {
+
+    static class Edge{
+        int src;
+        int dest;
+        public Edge(int src,int dest){
+            this.src = src;
+            this.dest = dest;
+        }
+        public static void createGraph(ArrayList<Edge> graph[]){
+            for(int i =0;i<graph.length;i++){
+                graph[i] = new ArrayList<>();
+            }
+            graph[0].add(new Edge(0,2));
+            graph[1].add(new Edge(1,2));
+            graph[1].add(new Edge(1,3));
+            graph[2].add(new Edge(2,3));
+            graph[2].add(new Edge(2,1));
+            graph[2].add(new Edge(2,0));
+            graph[3].add(new Edge(3,2));
+            graph[3].add(new Edge(3,1));
+        }
+
+        public static void main(String[] args) {
+            int v = 4; // no of nodes
+            ArrayList<Edge> graph[] = new ArrayList[v];
+            createGraph(graph);
+
+            //print the neighbours of node 2
+            for(int i =0;i<graph[2].size();i++){
+                Edge e = graph[2].get(i);
+                System.out.print(e.dest+" ");
+            }
+        }
+    }
+}
+
